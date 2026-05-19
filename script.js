c72d9b7be583018faa6436cc34f60a6eb2c4b6c9
Mathias Mayer <mayer.mathias@proton.me>
2026-05-19 08:54:55 +0200

    feat get From local storage

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function init() {
   renderBooks();
-  
-  
+  getFromLocalStorage();
 }
 
 function renderBooks() {
@@ -12,7 +11,6 @@ function renderBooks() {
 
     isLiked(i);
     publishedComments(i);
-    
   }
 }
 
@@ -33,8 +31,10 @@ function isLiked(i) {
     "/assets/icons/unliked.png");
   if (books[i].liked == true) {
     document.getElementById(`img${i}`).src = "/assets/icons/liked.png";
+    document.getElementById(`like-count${i}`).innerHTML = books[i].likes;
   } else {
     document.getElementById(`img${i}`).src = "/assets/icons/unliked.png";
+    document.getElementById(`like-count${i}`).innerHTML = books[i].likes;
   }
 }
 
@@ -45,11 +45,13 @@ function changeLike(i) {
     books[i].liked = false;
     books[i].likes--;
     document.getElementById(`like-count${i}`).innerHTML = books[i].likes;
+    saveToLocalStorage(i);
   } else {
     document.getElementById(`img${i}`).src = "/assets/icons/liked.png";
     books[i].liked = true;
     books[i].likes++;
     document.getElementById(`like-count${i}`).innerHTML = books[i].likes;
+    saveToLocalStorage(i);
   }
 }
 
@@ -66,18 +68,26 @@ function sendComment(i) {
   commentRef.value = "";
 }
 
-function saveToLocalStorage (i){
+function saveToLocalStorage(i) {
   localStorage.setItem(`likes${i}`, JSON.stringify(books[i].likes));
   localStorage.setItem(`liked${i}`, JSON.stringify(books[i].liked));
   localStorage.setItem(`comments${i}`, JSON.stringify(books[i].comments));
 }
 
-function getFromLocalStorage(){
-  let newLikes = JSON.parse(localStorage.getItem("likes"));
-  let newLiked = JSON.parse(localStorage.getItem("liked"));
-  let newComments = JSON.parse(localStorage.getItem("comments"));
-
-  books[i].likes = newLikes;
-  books[i].liked = newLiked;
-  books[i].comments = newComments;
+function getFromLocalStorage() {
+  for (let i = 0; i < books.length; i++) {
+    let newLikes = JSON.parse(localStorage.getItem(`likes${i}`));
+    let newLiked = JSON.parse(localStorage.getItem(`liked${i}`));
+    let newComments = JSON.parse(localStorage.getItem(`comments${i}`));
+    if (newComments == null) {
+      
+      publishedComments(i);
+    } else {
+      books[i].likes = newLikes;
+      books[i].liked = newLiked;
+      books[i].comments = newComments;
+      isLiked(i);
+      publishedComments(i);
+    }
+  }
 }
